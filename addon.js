0282a5b39c561d934e9feab0750465cfd14c9001
Ice Man <Iceman@LetItByte-MacBook.local>
2020-01-29 19:05:24 +0100

    remote data

--- addon.js
+++ addon.js
@@ -7,6 +7,7 @@ const manifest = {
 
   name: 'Rawl Movies',
   description: 'Por fin películas en español!!!',
+  logo: 'https://www.stremio.com/website/stremio-logo-small.png',
 
   //"icon": "URL to 256x256 monochrome png icon",
   //"background": "URL to 1024x786 png/jpg background",
@@ -20,11 +21,13 @@ const manifest = {
   catalogs: [
     {
       type: 'movie',
-      id: 'helloworldmovies'
+      id: 'helloworldmovies',
+      name: 'Peliculacas'
     },
     {
       type: 'series',
-      id: 'helloworldseries'
+      id: 'helloworldseries',
+      name: 'Seriacas'
     }
   ],
 
@@ -34,47 +37,19 @@ const manifest = {
 
 const builder = new addonBuilder(manifest);
 
-//Aladdin //https://torrentz2.eu/d835f77aec88288e8802c9ceec229c7a6a64dbf4
-//Toy story 4 //https://torrentz2.eu/653f8c79549a371a748ad4703a25c2a53cc43bca
-//Mascotas 2 //https://torrentz2.eu/a19daff4dface07dfe2f86bd8dcf606b6c514768
-//Dora //https://torrentz2.eu/49973634c7261ae6180ce20cbb5f9d1a38ff646d
-//IT 2//https://torrentz2.eu/4fe179e75f2c5e978157e144b021ab03ae391313
-
-const dataset = {
-  tt1560220: {
-    name: 'Zombieland 2',
-    type: 'movie',
-    infoHash: '0e0c4743b23a9fcf8e10b964f67b7700441afbcd'
-  },
-  tt6139732: {
-    name: 'Aladdin',
-    type: 'movie',
-    infoHash: 'd835f77aec88288e8802c9ceec229c7a6a64dbf4'
-  },
-  tt1979376: {
-    name: 'Toy Story 4',
-    type: 'movie',
-    infoHash: '653f8c79549a371a748ad4703a25c2a53cc43bca'
-  },
-  tt5113040: {
-    name: 'Mascotas 2',
-    type: 'movie',
-    infoHash: 'a19daff4dface07dfe2f86bd8dcf606b6c514768'
-  },
-  tt7547410: {
-    name: 'Dora y la ciudad perdida',
-    type: 'movie',
-    infoHash: '49973634c7261ae6180ce20cbb5f9d1a38ff646d'
-  },
-  tt7349950: {
-    name: 'IT 2',
-    type: 'movie',
-    infoHash: '4fe179e75f2c5e978157e144b021ab03ae391313'
-  }
-};
-
+// builder.defineStreamHandler(async function(args) {
+//   if (dataset[args.id]) {
+//     return Promise.resolve({ streams: [dataset[args.id]] });
+//   } else {
+//     return Promise.resolve({ streams: [] });
+//   }
+// });
 builder.defineStreamHandler(async function(args) {
+  let file = await axios.get('https://api.myjson.com/bins/oz3se');
+  let dataset = file.data;
+
   if (dataset[args.id]) {
+    defineCatalog();
     return Promise.resolve({ streams: [dataset[args.id]] });
   } else {
     return Promise.resolve({ streams: [] });
@@ -96,13 +71,17 @@ const generateMetaPreview = function(value, key) {
   };
 };
 
-builder.defineCatalogHandler(function(args, cb) {
-  // filter the dataset object and only take the requested type
+builder.defineCatalogHandler(defineCatalog);
+
+async function defineCatalog(args, cb) {
+  let file = await axios.get('https://api.myjson.com/bins/oz3se');
+  let dataset = file.data;
+
   const metas = Object.entries(dataset)
     .filter(([_, value]) => value.type === args.type)
     .map(([key, value]) => generateMetaPreview(value, key));
 
   return Promise.resolve({ metas: metas });
-});
+}
 
 module.exports = builder.getInterface();
---
+++ dataset.js
@@ -0,0 +1,144 @@
+const dataset = {
+  tt4154664: {
+    name: 'Rawl Movies',
+    data: 'Capitana Marvel',
+    type: 'movie',
+    infoHash: '29b69ff44e03075e4db6db03c3c25ef7428bbb4c'
+  },
+  tt1560220: {
+    name: 'Rawl Movies',
+    data: 'Zombieland 2',
+    type: 'movie',
+    infoHash: '0e0c4743b23a9fcf8e10b964f67b7700441afbcd'
+  },
+  tt6139732: {
+    name: 'Rawl Movies',
+    data: 'Aladdin',
+    type: 'movie',
+    infoHash: 'd835f77aec88288e8802c9ceec229c7a6a64dbf4'
+  },
+  tt1979376: {
+    name: 'Rawl Movies',
+    data: 'Toy Story 4',
+    type: 'movie',
+    infoHash: '653f8c79549a371a748ad4703a25c2a53cc43bca'
+  },
+  tt5113040: {
+    name: 'Rawl Movies',
+    data: 'Mascotas 2',
+    type: 'movie',
+    infoHash: 'a19daff4dface07dfe2f86bd8dcf606b6c514768'
+  },
+  tt7547410: {
+    name: 'Rawl Movies',
+    data: 'Dora y la ciudad perdida',
+    type: 'movie',
+    infoHash: '49973634c7261ae6180ce20cbb5f9d1a38ff646d'
+  },
+  tt7349950: {
+    name: 'Rawl Movies',
+    data: 'IT 2',
+    type: 'movie',
+    infoHash: '4fe179e75f2c5e978157e144b021ab03ae391313'
+  },
+  tt6105098: {
+    name: 'Rawl Movies',
+    data: 'El Rey León',
+    type: 'movie',
+    infoHash: '61f30d7d181f97f9b9edfaf976de15b53fdb2f8c'
+  },
+  tt2935510: {
+    name: 'Rawl Movies',
+    data: 'Ad Astra',
+    type: 'movie',
+    infoHash: '443bd60999fe0d9448249495ed85d3ac97fc9668'
+  },
+  tt6320628: {
+    name: 'Rawl Movies',
+    data: 'Spiderman Lejos de Casa',
+    type: 'movie',
+    infoHash: 'e814ee6ffbb8fe00c9412a74cfda1b385e71f474'
+  },
+  tt6806448: {
+    name: 'Rawl Movies',
+    data: 'Fast & Furious 9',
+    type: 'movie',
+    infoHash: 'e88e20eb4eb2f141f374f2cd4d347dc9b23ae2d2'
+  },
+  tt1598778: {
+    name: 'Rawl Movies',
+    data: 'Contagio',
+    type: 'movie',
+    infoHash: 'e7a13fa50699c69b7bba0cd42944fcdbb98ca2eb'
+  },
+  tt6565702: {
+    name: 'Rawl Movies',
+    data: 'X-Men: Fénix Oscura',
+    type: 'movie',
+    infoHash: 'c5faedab57dfcf4f7ec34ab94c1222c2cd75d208'
+  },
+  tt1727824: {
+    name: 'Rawl Movies',
+    data: 'Bohemian Rhapsody',
+    type: 'movie',
+    infoHash: '7ebce3bd80ce2c0e75d7614ef7fc7a284fd2b972'
+  },
+  tt3861390: [
+    {
+      name: 'Rawl Movies',
+      data: 'Dumbo',
+      type: 'movie',
+      infoHash: 'be4250b32611c0e09471d985864a661493894b38'
+    },
+    {
+      name: 'Rawl Movies 4K',
+      data: 'Dumbo',
+      type: 'movie',
+      infoHash: 'be4250b32611c0e09471d985864a661493894b38'
+    }
+  ],
+  tt1477834: {
+    name: 'Rawl Movies',
+    data: 'Aquaman',
+    type: 'movie',
+    infoHash: '26f50edbf89b5f5bdff50a6c63827b7089f2925f'
+  },
+  'tt2306299:6:1': {
+    name: 'Rawl Movies',
+    data: 'Vikingos',
+    type: 'series',
+    infoHash: 'bd739f0642e6d7def711f5fb715d41c3a8a9c379'
+  },
+  'tt2306299:6:2': {
+    name: 'Rawl Movies',
+    data: 'Vikingos',
+    type: 'series',
+    infoHash: 'dc65d547f8353cb09c5bc986d4b991caf61799a8'
+  },
+  'tt2306299:6:3': {
+    name: 'Rawl Movies',
+    data: 'Vikingos',
+    type: 'series',
+    infoHash: 'c98b21ad5fd9da31a29ca5e04ecc4fdce7e4a806'
+  },
+  'tt2306299:6:4': {
+    name: 'Rawl Movies',
+    data: 'Vikingos',
+    type: 'series',
+    infoHash: '7f039c153b4707ee721a63f6d35b9efa0de5003d'
+  },
+  'tt2306299:6:5': {
+    name: 'Rawl Movies',
+    data: 'Vikingos',
+    type: 'series',
+    infoHash: '53202f1b740ebd87b548dbc235e5d95518d3eab3'
+  },
+  'tt2306299:6:6': {
+    name: 'Rawl Movies',
+    data: 'Vikingos',
+    type: 'series',
+    infoHash: '8184460f0415e3c2faeb7e22645ee927a1e4f39d'
+  }
+};
+
+module.exports = dataset;
